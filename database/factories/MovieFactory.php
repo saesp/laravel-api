b4097cd7815bb219bfe131c709885c126b08c0f8
esposito.00@outlook.it <esposito.00@outlook.it>
2023-02-15 18:38:21 +0100

    Factories

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Core\Number;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake() -> title(),
+            'original_title' => fake() -> title(),
+            'code' => fake() -> regexify('[A-Z0-9]{5}'),
+            'plot' => fake()->boolean() ? fake()->text() : null,
+            'vote' => fake()->boolean() ? fake()->randomFloat(1, 0, 5) : null,
+            'release_date' => fake() -> dateTimeBetween("-100 years", 'now', null),
+        ];
+    }
+}

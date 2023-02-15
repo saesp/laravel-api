f10059cc41ca22d1f590af293241da8a589e0697
esposito.00@outlook.it <esposito.00@outlook.it>
2023-02-15 17:24:41 +0100

    first commit

---
+++ database/migrations/2023_02_15_153011_create_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+
+            $table -> string('title', 64);
+            $table -> string('original_title', 64);
+            $table -> string('code', 5) -> unique();
+            $table -> text('plot') -> nullable();
+            $table -> float('vote') -> unsigned() -> default(0) -> nullable();
+            $table -> date('release_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
